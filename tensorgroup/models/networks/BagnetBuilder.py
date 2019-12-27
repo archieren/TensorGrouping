7564b497442bd4cda36ae9850e21713db61c578c
archieren <renjianpeng@gmail.com>
2019-12-27 14:23:19 +0800

    Coding...

--- tensorgroup/models/networks/BagnetBuilder.py
+++ tensorgroup/models/networks/BagnetBuilder.py
@@ -13,6 +13,9 @@ ROW_AXIS = 1
 COL_AXIS = 2
 CHANNEL_AXIS = 3
 
+# 有几件事需要说一下：
+# 1) 残差网中,padding用的都是"same",而不是这的"valid"
+# 2) 修改了shortcut里的内容，参照作者pytorch的实现！
 
 def _bn_relu(input):
     """Helper to build a BN -> relu block
@@ -28,7 +31,7 @@ def _conv_bn_relu(**conv_params):
     kernel_size = conv_params["kernel_size"]
     strides = conv_params.setdefault("strides", (1, 1))
     kernel_initializer = conv_params.setdefault("kernel_initializer", "he_normal")
-    padding = conv_params.setdefault("padding", "same")
+    padding = conv_params.setdefault("padding", "valid")
     kernel_regularizer = conv_params.setdefault("kernel_regularizer", KR.l2(1.e-4))
 
     def f(input):
@@ -41,6 +44,8 @@ def _conv_bn_relu(**conv_params):
     return f
 
 
+
+
 def _bn_relu_conv(**conv_params):
     """Helper to build a BN -> relu -> conv block.
     This is an improved scheme proposed in http://arxiv.org/pdf/1603.05027v2.pdf
@@ -49,7 +54,7 @@ def _bn_relu_conv(**conv_params):
     kernel_size = conv_params["kernel_size"]
     strides = conv_params.setdefault("strides", (1, 1))
     kernel_initializer = conv_params.setdefault("kernel_initializer", "he_normal")
-    padding = conv_params.setdefault("padding", "same")
+    padding = conv_params.setdefault("padding", "valid")
     kernel_regularizer = conv_params.setdefault("kernel_regularizer", KR.l2(1.e-4))
 
     def f(input):
@@ -70,7 +75,7 @@ def _shortcut(input, residual):
     # Should be int if network architecture is correctly configured.
     input_shape = KB.int_shape(input)
     residual_shape = KB.int_shape(residual)
-    print("{}.{}".format(input_shape,residual_shape))
+    # print("{}.{}".format(input_shape,residual_shape))
     stride_width = int(round(input_shape[ROW_AXIS] / residual_shape[ROW_AXIS]))
     stride_height = int(round(input_shape[COL_AXIS] / residual_shape[COL_AXIS]))
     equal_channels = input_shape[CHANNEL_AXIS] == residual_shape[CHANNEL_AXIS]
@@ -79,7 +84,6 @@ def _shortcut(input, residual):
     # 1 X 1 conv if shape is different. Else identity.
     # In the first bottleneck of each _residual_block, this will be in case !
     # 每一残差块的第一个瓶颈层，会出现这种情况？
-    # 原始论文的Pytorch实现，搞得太麻烦了！
     if stride_width > 1 or stride_height > 1 or not equal_channels:
         shortcut = KL.Conv2D(filters=residual_shape[CHANNEL_AXIS],
                              kernel_size=(1, 1),
@@ -87,7 +91,10 @@ def _shortcut(input, residual):
                              padding="valid",
                              kernel_initializer="he_normal",
                              kernel_regularizer=KR.l2(0.0001))(input)
-
+        shortcut = shortcut[:,:residual_shape[ROW_AXIS],:residual_shape[ROW_AXIS],:]
+    
+    print(shortcut.shape)
+    print(residual.shape)
     return KL.add([shortcut, residual])
 
 
@@ -96,7 +103,7 @@ def _residual_block(block_function, filters, repetitions, is_kernel_3, k3_stride
     is_kernel_3 means many things for the first block
     """
     def f(input):
-        # 第一个瓶颈层,有些特殊,而且和他的原型Resnet中对应的层处理的方式有差别！
+        # 第一个瓶颈层,有些特殊,它的stride和kernel_size是要根据配置来调整的,而且和他的原型Resnet中对应的层处理的方式有差别！
         # 即kernel和stride的设置有区别！
         # 参见ResnetBuilder.py内的注释.
         input = block_function(filters,k3_stride= k3_stride, is_kernel_3= is_kernel_3)(input)
@@ -107,6 +114,29 @@ def _residual_block(block_function, filters, repetitions, is_kernel_3, k3_stride
     return f
 
 
+def _conv_bn_residual_relu(**conv_params): 
+    """Helper to build a conv -> BN ->Residual -> Relu block
+    这是我根据Bagnet里的要求改的！在Bagnet里，residual在Relu前.
+    """
+    init_input = conv_params["input"]
+    filters = conv_params["filters"]
+    kernel_size = conv_params["kernel_size"]
+    strides = conv_params.setdefault("strides", (1, 1))
+    kernel_initializer = conv_params.setdefault("kernel_initializer", "he_normal")
+    padding = conv_params.setdefault("padding", "valid")
+    kernel_regularizer = conv_params.setdefault("kernel_regularizer", KR.l2(1.e-4))
+
+    def f(input):
+        conv = KL.Conv2D(filters=filters, kernel_size=kernel_size,
+                         strides=strides, padding=padding,
+                         kernel_initializer=kernel_initializer,
+                         kernel_regularizer=kernel_regularizer)(input)
+        norm = KL.BatchNormalization(axis=CHANNEL_AXIS)(conv)
+        residual = _shortcut(init_input, norm)
+
+        return KL.Activation("relu")(residual)
+
+    return f
 
 def bottleneck(filters, is_kernel_3 = False, k3_stride = 1):
     """
@@ -122,28 +152,27 @@ def bottleneck(filters, is_kernel_3 = False, k3_stride = 1):
 
         conv_1_1 = _conv_bn_relu(filters=filters, kernel_size=(1, 1))(input)
         conv_3_3 = _conv_bn_relu(filters=filters, kernel_size=(k3_size,k3_size),strides=(k3_stride, k3_stride))(conv_1_1)
-        residual = _conv_bn_relu(filters=filters * 4, kernel_size=(1, 1))(conv_3_3)
-        return _shortcut(input, residual)
+        residual = _conv_bn_residual_relu(input= input,filters=filters * 4, kernel_size=(1, 1))(conv_3_3)
+        return residual # _shortcut(input, residual)
 
     return f
 
 
 class BagnetBuilder(object):
     @staticmethod
-    def build(input_shape, num_outputs, block_fn, repetitions,k3,strides):
+    def build(input_shape, num_outputs, repetitions,k3,strides):
         """Builds a custom ResNet like architecture.
 
         Args:
             input_shape: The input shape in the form (nb_channels, nb_rows, nb_cols)
             num_outputs: The number of outputs at final softmax layer
-            block_fn: The block function to use. This is either `basic_block` or `bottleneck`.
-                The original paper used basic_block for layers < 50
             repetitions: Number of repetitions of various block units.
                 At each block unit, the number of filters are doubled and the input size is halved
 
         Returns:
             The keras `Model`.
         """
+        block_fn = bottleneck
         if len(input_shape) != 3:
             raise Exception("Input shape should be a tuple (nb_channels, nb_rows, nb_cols)")
         
@@ -176,7 +205,7 @@ class BagnetBuilder(object):
             filters *= 2
 
         # Last activation
-        block = _bn_relu(block)
+        # block = _bn_relu(block) # 没必要了！
 
         # Classifier block
         block_shape = KB.int_shape(block)
@@ -194,7 +223,7 @@ class BagnetBuilder(object):
     def build_bagnet_9(input_shape, num_outputs):
         return BagnetBuilder.build(input_shape, 
                                     num_outputs, 
-                                    bottleneck, 
+                                    #bottleneck, 
                                     repetitions= [3, 4, 6, 3],
                                     k3= [1, 1, 0, 0],
                                     strides=[2, 2, 2, 1])
@@ -203,7 +232,7 @@ class BagnetBuilder(object):
     def build_bagnet_17(input_shape, num_outputs):
         return BagnetBuilder.build(input_shape, 
                                     num_outputs, 
-                                    bottleneck, 
+                                    #bottleneck, 
                                     repetitions= [3, 4, 23, 3],
                                     k3 = [1, 1, 1, 0],
                                     strides = [2, 2, 2, 1])
@@ -212,7 +241,7 @@ class BagnetBuilder(object):
     def build_bagnet_33(input_shape, num_outputs):
         return BagnetBuilder.build(input_shape, 
                                     num_outputs, 
-                                    bottleneck, 
+                                    #bottleneck, 
                                     repetitions = [3, 8, 36, 3],
                                     k3 = [1 , 1, 1, 1],
                                     strides = [2, 2, 2, 1])
--- try.py
+++ try.py
@@ -1,7 +1,66 @@
+
+import multiprocessing
+
+from matplotlib import pyplot as plt
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+
 from  tensorgroup.models.networks.ResnetKeypointBuilder import ResnetKeypointBuilder as RKB
 from tensorgroup.models.networks.BagnetBuilder import BagnetBuilder as BB
 
-image_shape = (512,512,3) #(224, 224, 3)
+#image_shape = (33, 33, 3)
 #modelP = RKB.build_pose_resnet_50(input_shape = image_shape, num_outputs = 10)
-modelB = BB.build_bagnet_9(input_shape=image_shape, num_outputs=10)
-modelB.summary()
+#modelP.summary()
+#modelB = BB.build_bagnet_33(input_shape=image_shape, num_outputs=1000)
+#modelB.summary()
+
+"""
+FeaturesDict({
+    'file_name': Text(shape=(), dtype=tf.string),
+    'image': Image(shape=(None, None, 3), dtype=tf.uint8),
+    'label': ClassLabel(shape=(), dtype=tf.int64, num_classes=1000),
+})
+"""
+
+print(tfds.list_builders())
+
+mnist_train, info_mnist_train= tfds.load(name="mnist", split="train", with_info= True)
+print(info_mnist_train)
+assert isinstance(mnist_train, tf.data.Dataset)
+
+imagenet2012_train, info_train = tfds.load( name="imagenet2012"
+                                            , split="train"
+                                            , with_info=True
+                                            #, decoders={'image': tfds.decode.SkipDecoding(),}
+                                            )
+
+imagenet2012_val, info_val = tfds.load( name="imagenet2012"
+                                        , split="validation"
+                                        , with_info=True
+                                        #, decoders={'image': tfds.decode.SkipDecoding(),}
+                                        )
+assert isinstance(imagenet2012_train, tf.data.Dataset)
+print(info_train)
+print(info_val)
+
+print( multiprocessing.cpu_count())
+
+def d_scale(record):
+    image, label = record['image'], record['label']    
+    image = tf.cast(image, tf.float32)
+    image /= 255
+    return image, label
+
+imagenet2012_train_pro = imagenet2012_train.map(d_scale)
+print(imagenet2012_train_pro.element_spec)
+#print(imagenet2012_train.output_types)
+#print(imagenet2012_train.output_shape)
+#for example in imagenet2012_train.take(10):
+    #image = example['image']
+    #print(repr(example))
+    #plt.imshow(image)
+    #plt.show()
+
+
